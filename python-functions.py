17b0ef9b2b35decf2a5eba5dd5b7d52b56e40659
ayanbajo <bolatitoayanbajo@gmail.com>
2022-11-30 19:33:31 -0500

    started assignment

---
+++ python-functions.py
@@ -0,0 +1,22 @@
+def max_num(x,y,z):
+    return max([x,y,z])
+print(max_num(20,30,40))
+
+def mult_list(list):
+    total = 1
+    for i in list:
+        total *= i
+    return total
+print(mult_list((1,2,3,4,5)))
+
+def rev_string(string):
+    return string[::-1]
+print(rev_string("God is Good"))
+
+def num_within(a,b,c):
+    return a in range(a,b,c)
+print(num_within(5,2,3))
+
+def pascal(n):
+
+
